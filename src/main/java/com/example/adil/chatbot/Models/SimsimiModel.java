dbb4773e270bc941db24f5c342ab2106af447664
Adil Deshmukh <desadi96@gmail.com>
2018-04-27 10:07:06 +0530

    added files

---
+++ src/main/java/com/example/adil/chatbot/Models/SimsimiModel.java
@@ -0,0 +1,56 @@
+package com.example.adil.chatbot.Models;
+
+/**
+ * Created by Adil on 22-Apr-18.
+ */
+
+public class SimsimiModel {
+
+    public String response;
+    public String id;
+    public int result;
+    public String msg;
+
+    public SimsimiModel(String response, String id, int result, String msg) {
+        this.response = response;
+        this.id = id;
+        this.result = result;
+        this.msg = msg;
+    }
+
+    public SimsimiModel() {
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
